87b57871837903a9519d18b029d3e4bb6f1390a9
Juho Friman <juho.friman@solita.fi>
2016-11-16 17:17:26 +0200

    Basic proxying can now be done

--- src/channeler/handler.clj
+++ src/channeler/handler.clj
@@ -1,14 +1,60 @@
 (ns channeler.handler
   (:gen-class)
-  (:require [org.httpkit.server :as httpkit]))
+  (:require [org.httpkit.server :as httpkit]
+            [org.httpkit.client :as httpclient]))
 
-(defn app [req]
-  (println req)
-  {:status  200
-   :headers {"Content-Type" "text/html"}
-   :body    "hello HTTP!"})
+(def routes (atom {}))
+
+(defn split-uri
+  [uri]
+  (let [parts (clojure.string/split uri #"/")]
+    (reduce #(if (= "*" %2) (conj %1 :all) (conj %1 %2)) [:all] (rest parts))))
+
+(defn register-route!
+  [uri target]
+  (swap! routes #(assoc % uri target)))
+
+; this is not matching uri, but matching route key in routespec
+(defn matching-uri
+  [uris uri]
+  (reduce #(if (.startsWith uri %2) %2 %1) nil uris))
+
+(defn build-url
+  [uri {:keys [host port] :as spec}]
+  (if (.startsWith uri "/")
+    (str "http://" host ":" port uri)
+    (str "http://" host ":" port "/" uri)))
+
+(defn proxy-url [routespec uri]
+  (if-let [uri-key (matching-uri (keys routespec) uri)] 
+    (build-url (subs uri (count uri-key)) (get routespec uri-key))))
+
+(defn reset-routes! [] (reset! routes {}))
+
+(defn process-proxy
+  [{:keys [status headers body error]}]
+  (if error
+    {:status 404
+     :headers {"content-type" "text/plain"}
+     :body (str "Error: " error)}
+    {:status status
+     :headers (reduce-kv #(assoc %1 (name %2) %3) {} headers)
+     :body body}))
+
+(defn app [{uri :uri}]  
+  (if-let [route (proxy-url @routes uri)]
+    (do  (println (str uri ": Proxying "  route))
+         @(httpclient/get
+           route
+           process-proxy))
+    {:status 404
+     :headers {"content-type" "text/plain"}
+     :body (str "No such route " uri)}))
+
+
 
 (defonce server (atom nil))
+
 
 (defn stop-server []
   (when-not (nil? @server)
@@ -21,4 +67,4 @@
   ;; The #' is useful when you want to hot-reload code
   ;; You may want to take a look: https://github.com/clojure/tools.namespace
   ;; and http://http-kit.org/migration.html#reload
-  (reset! server (httpkit/run-server #'app {:port 8080})))
+  (reset! server (httpkit/run-server #'app {:port 1234})))
---
+++ test/channeler/handler_test.clj
@@ -0,0 +1,23 @@
+(ns channeler.handler-test
+  (:require [clojure.test :refer :all]
+            [channeler.handler :refer :all]))
+
+
+(deftest get-matching-uri
+  (testing "testing getting matching uri"
+    (is (= "/" (matching-uri ["/" "/foo"] "/")))
+    (is (= "/" (matching-uri ["/" "/foo"] "/baz")))
+    (is (= "/foo" (matching-uri ["/" "/foo" "/baz"] "/foo")))
+    (is (= "/foo" (matching-uri ["/" "/foo" "/baz"] "/foo/bar")))))
+
+(deftest route-extraction
+  (testing "getting proxied urls from routespec"
+    (is (nil? (proxy-url {} "/")))
+    (is (= "http://localhost:1234/"
+           (proxy-url {"/" {:host "localhost" :port 1234 :path "*"}} "/")))
+    (is (= "http://localhost:1234/foobarbaz"
+           (proxy-url {"/" {:host "localhost" :port 1234 :path "*"}} "/foobarbaz")))
+    (is (= "http://localhost:1234/bar"
+           (proxy-url {"/foo" {:host "localhost" :port 1234 :path "*"}} "/foo/bar")))))
+
+
